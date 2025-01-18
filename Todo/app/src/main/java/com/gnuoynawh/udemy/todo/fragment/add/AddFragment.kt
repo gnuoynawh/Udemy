76aadd1b40f0d1edbbe789809c4ff3892c137389
gnuoynawh <bizxhy@gmail.com>
2025-01-18 13:29:36 +0900

    Navigation, Fragment 생성

---
+++ Todo/app/src/main/java/com/gnuoynawh/udemy/todo/fragment/add/AddFragment.kt
@@ -0,0 +1,12 @@
+package com.gnuoynawh.udemy.todo.fragment.add
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class AddFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
