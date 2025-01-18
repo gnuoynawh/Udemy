cf3e3d0c06045eaf71b29fb9e092ca0d33ed4e21
gnuoynawh <bizxhy@gmail.com>
2025-01-18 13:51:56 +0900

    리소스 추가, ListFragment UI 생성

--- Todo/app/src/main/java/com/gnuoynawh/udemy/todo/fragment/list/ListFragment.kt
+++ Todo/app/src/main/java/com/gnuoynawh/udemy/todo/fragment/list/ListFragment.kt
@@ -5,11 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.gnuoynawh.udemy.todo.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        view.findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return view
     }
 
 }
--- Todo/app/src/main/java/com/gnuoynawh/udemy/todo/fragment/update/UpdateFragment.kt
+++ Todo/app/src/main/java/com/gnuoynawh/udemy/todo/fragment/update/UpdateFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.gnuoynawh.udemy.todo.R
 
 class UpdateFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update, container, false)
     }
 
 }
