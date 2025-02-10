6e32f7ad765b5ced03e87752d854ca498f0f3de7
gnuoynawh <bizxhy@gmail.com>
2025-02-10 21:01:12 +0900

    DataBinding 적용 (ListFragment)

---
+++ Todo/app/src/main/java/com/gnuoynawh/udemy/todo/fragment/BindAdapter.kt
@@ -0,0 +1,33 @@
+package com.gnuoynawh.udemy.todo.fragment
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.MutableLiveData
+import androidx.navigation.findNavController
+import com.gnuoynawh.udemy.todo.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class BindAdapter {
+
+    companion object {
+
+        @BindingAdapter("android:navigateToAddFragment")
+        @JvmStatic
+        fun navigateToAddFragment(view: FloatingActionButton, navigate: Boolean) {
+            view.setOnClickListener {
+                if (navigate) {
+                    view.findNavController().navigate(R.id.action_listFragment_to_addFragment)
+                }
+            }
+        }
+
+        @BindingAdapter("android:emptyDatabase")
+        @JvmStatic
+        fun emptyDatabase(view: View, emptyDatabase: MutableLiveData<Boolean>) {
+            when(emptyDatabase.value!!) {
+                true -> view.visibility = View.VISIBLE
+                false -> view.visibility = View.INVISIBLE
+            }
+        }
+    }
+}
--- Todo/app/src/main/java/com/gnuoynawh/udemy/todo/fragment/list/ListFragment.kt
+++ Todo/app/src/main/java/com/gnuoynawh/udemy/todo/fragment/list/ListFragment.kt
@@ -13,11 +13,13 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.gnuoynawh.udemy.todo.R
 import com.gnuoynawh.udemy.todo.data.viewmodel.TodoViewModel
+import com.gnuoynawh.udemy.todo.databinding.FragmentListBinding
 import com.gnuoynawh.udemy.todo.fragment.SharedViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -26,38 +28,33 @@ class ListFragment : Fragment() {
     private val mToDoViewModel: TodoViewModel by viewModels()
     private val mSharedViewModel: SharedViewModel by viewModels()
 
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
     private val adapter: ListAdapter by lazy { ListAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
 
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+        // Data Binding
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.mSharedViewModel = mSharedViewModel
 
+        //set RecyclerView
+        setRecyclerView()
+
+        // Observe LiveData
         mToDoViewModel.getAllData.observe(viewLifecycleOwner) { data ->
             mSharedViewModel.checkIfDatabaseEmpty(data)
             adapter.setData(data)
         }
-        mSharedViewModel.emptyDatabase.observe(viewLifecycleOwner) {
-            showEmptyDatabaseViews(it)
-        }
-
-        view.findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener {
-            findNavController().navigate(R.id.action_listFragment_to_addFragment)
-        }
-//
-//        view.findViewById<ConstraintLayout>(R.id.listLayout).setOnClickListener {
-//            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
-//        }
 
         //set Menu
         setHasOptionsMenu(true)
 
-        return view
+        return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -71,14 +68,10 @@ class ListFragment : Fragment() {
         return super.onOptionsItemSelected(item)
     }
 
-    private fun showEmptyDatabaseViews(emptyDatabase: Boolean) {
-        if (emptyDatabase) {
-            view?.findViewById<TextView>(R.id.no_data_textView)?.visibility = View.VISIBLE
-            view?.findViewById<ImageView>(R.id.no_data_imageView)?.visibility = View.VISIBLE
-        } else {
-            view?.findViewById<TextView>(R.id.no_data_textView)?.visibility = View.INVISIBLE
-            view?.findViewById<ImageView>(R.id.no_data_imageView)?.visibility = View.INVISIBLE
-        }
+    private fun setRecyclerView() {
+        val recyclerView = binding.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
     }
 
     private fun confirmRemoval() {
@@ -96,5 +89,10 @@ class ListFragment : Fragment() {
             .setMessage("Are you sure you want to remove everything?")
             .create()
             .show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
